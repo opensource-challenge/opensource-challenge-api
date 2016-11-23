8c4f657636a94cc0cf03c097c46b4360ec88f614
Damian Senn <damian.senn@topaxi.codes>
2016-11-23 11:39:17 +0100

    Add task to create initial user

---
+++ lib/mix/tasks/opensource_challenge.create_admin.ex
@@ -0,0 +1,70 @@
+defmodule Mix.Tasks.OpensourceChallenge.CreateAdmin do
+  use Mix.Task
+  import Mix.Ecto
+
+  alias OpensourceChallenge.User
+
+  @shortdoc "Creates a new admin user"
+
+  @moduledoc """
+    Creates a new admin user to manage the OpenSource Challenges
+  """
+
+  def run(args) do
+    [ repo ] = parse_repo(args)
+
+    ensure_repo(repo, args)
+    ensure_started(repo, [])
+
+    username = Mix.shell.prompt("Username:") |> String.strip
+    email    = Mix.shell.prompt("Email:") |> String.strip
+    password = password_get("Password:") |> String.strip
+    confirm  = password_get("Password (confirm):") |> String.strip
+
+    if password != confirm do
+      Mix.raise "Entered passwords do not match"
+    end
+
+    changeset = User.changeset(%User{}, %{
+                                 name: username,
+                                 email: email,
+                                 password: password,
+                                 password_confirmation: confirm
+                               })
+
+    repo.insert! changeset
+  end
+
+  # Password prompt that hides input by every 1ms
+  # clearing the line with stderr
+  def password_get(prompt) do
+    if Hex.State.fetch!(:clean_pass) do
+      password_clean(prompt)
+    else
+      Mix.shell.prompt(prompt <> " ")
+    end
+  end
+
+  defp password_clean(prompt) do
+    pid   = spawn_link(fn -> loop(prompt) end)
+    ref   = make_ref()
+    value = IO.gets(prompt <> " ")
+
+    send pid, {:done, self(), ref}
+    receive do: ({:done, ^pid, ^ref}  -> :ok)
+
+    value
+  end
+
+  defp loop(prompt) do
+    receive do
+      {:done, parent, ref} ->
+        send parent, {:done, self(), ref}
+        IO.write(:standard_error, "\e[2K\r")
+    after
+      1 ->
+        IO.write(:standard_error, "\e[2K\r#{prompt} ")
+        loop(prompt)
+    end
+  end
+end
