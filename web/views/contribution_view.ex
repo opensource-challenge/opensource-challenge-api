32e90df95837747aa4dfda0a5f889e3a51b33a42
Damian Senn <senn@puzzle.ch>
2016-12-13 17:28:09 +0100

    Add challenge link to contributions

--- web/views/contribution_view.ex
+++ web/views/contribution_view.ex
@@ -14,7 +14,16 @@ defmodule OpensourceChallenge.ContributionView do
     identifiers: :when_included,
     link: :user_link
 
+  has_one :challenge,
+    serializer: OpensourceChallenge.ChallengeView,
+    identifiers: :when_included,
+    link: :challenge_link
+
   def user_link(contribution, conn) do
     user_url(conn, :show, contribution.user_id)
+  end
+
+  def challenge_link(contribution, conn) do
+    challenge_url(conn, :show, contribution.challenge_id)
   end
 end
