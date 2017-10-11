a279df539405147f75718db3a75f3a5a2956b480
Damian Senn <senn@puzzle.ch>
2017-10-11 16:18:58 +0200

    Update ja_serializer and ja_resource

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -5,26 +5,41 @@ defmodule OpensourceChallenge.ErrorViewTest do
 
   test "renders 404.json" do
     assert render(OpensourceChallenge.ErrorView, "404.json", []) ==
-           %{"errors" => [%{code: 404, title: "Page Not Found"}]}
+      %{
+        "errors" => [%{code: 404, title: "Page Not Found"}],
+        "jsonapi" => %{"version" => "1.0"},
+      }
   end
 
   test "renders 403.json" do
     assert render(OpensourceChallenge.ErrorView, "403.json", []) ==
-           %{"errors" => [%{code: 403, title: "Forbidden"}]}
+      %{
+        "errors" => [%{code: 403, title: "Forbidden"}],
+        "jsonapi" => %{"version" => "1.0"},
+      }
   end
 
   test "renders 415.json" do
     assert render(OpensourceChallenge.ErrorView, "415.json", []) ==
-           %{"errors" => [%{code: 415, title: "Unsupported Media Type"}]}
+      %{
+        "errors" => [%{code: 415, title: "Unsupported Media Type"}],
+        "jsonapi" => %{"version" => "1.0"},
+      }
   end
 
   test "render 500.json" do
     assert render(OpensourceChallenge.ErrorView, "500.json", []) ==
-           %{"errors" => [%{code: 500, title: "Internal Server Error"}]}
+      %{
+        "errors" => [%{code: 500, title: "Internal Server Error"}],
+        "jsonapi" => %{"version" => "1.0"},
+      }
   end
 
   test "render any other" do
     assert render(OpensourceChallenge.ErrorView, "505.json", []) ==
-           %{"errors" => [%{code: 505, title: "Internal Server Error"}]}
+      %{
+        "errors" => [%{code: 505, title: "Internal Server Error"}],
+        "jsonapi" => %{"version" => "1.0"},
+      }
   end
 end
